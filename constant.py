5a02b96766f7fcf2e2eb6d78084b90a50b743f34
Carmine Choi <carmine.choi@gmail.com>
2021-02-16 08:58:24 -0800

    first commit

---
+++ constant.py
@@ -0,0 +1,9 @@
+# login credentials
+USERNAME = 'MapleStaples72'
+PASSWORD = 'Gogosing123'
+
+# tokens
+CLIENT_ID = 'qC8dnHaxvHMKSA'
+SECRET_TOKEN = 'pthJfFN4vmdwDUt_2K3XvTCYmgOspg'
+
+USER_AGENT = 'ToTheMoon:0.0.1 (by /u/MapleStaples72'
